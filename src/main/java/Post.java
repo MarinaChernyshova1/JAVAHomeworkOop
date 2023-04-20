407d4f18aa0ff22ff6efa7ecb3f4cc9790863e2c
MarinaCh <marina3123273@mail.ru>
2023-04-20 14:24:24 +0300

    Initial coomit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String surname;
+    public String numberPhone;
+    public boolean subscription;
+
+
+
+}
